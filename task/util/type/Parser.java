a0f8233fefc4a62c4cdc742419d169575aea22fe
bvaudour <bvaudour@myopera.com>
2013-10-13 15:43:45 +0200

    Convertisseurs multiples

--- task/util/type/Parser.java
+++ task/util/type/Parser.java
@@ -3,20 +3,28 @@ package task.util.type;
 import task.util.generic.T3;
 
 /**
- * Created with IntelliJ IDEA.
- * User: benjamin
- * Date: 06/10/13
- * Time: 15:23
- * To change this template use File | Settings | File Templates.
+ * La classe <code>Parser</code> regroupe un ensemble de méthodes permettant de convertir des données d'un type à un autre
+ * @author Benjamin VAUDOUR
+ * @since 1.0
  */
 public class Parser {
 
-  // Conversion String en int
+  /**
+   * Convertit un chaîne de caractères en entier
+   * @param s Chaîne à convertir
+   * @return Entier correspondant (0 si la chaîne ne correspond pas à un format d'entier)
+   */
   public static int toInt(String s) {
     return (s != null && s.matches("\\d+")) ? Integer.valueOf(s) : 0;
   }
 
-  // Conversion int en String numérique de longueur nbDigit
+  /**
+   * Convertit un entier en chaîne numérique contenant un nombre de chiffres donnés
+   * @param b Chaîne à modifier
+   * @param i Entier à convertir
+   * @param nbDigits Nombre de chiffres que doit contenir la chaîne
+   * @return Chaîne modifiée
+   */
   public static StringBuilder formatInt(StringBuilder b, int i, int nbDigits) {
     int p10 = 1;
     while (--nbDigits > 0) {
@@ -26,7 +34,11 @@ public class Parser {
     return b.append(i);
   }
 
-  // Conversion String en date (dans l'ordre : y, m, d)
+  /**
+   * Convertit une chaîne de caractère en triplé d'entiers correspondants à une date
+   * @param s Chaîne à convertir
+   * @return Triplé d'entiers contenant l'année, le mois et le jour du mois (Triplé de date nulle si chaîne au mauvais format)
+   */
   public static T3<Integer, Integer, Integer> toDate(String s) {
     T3<Integer, Integer, Integer> d = Date.DNULL();
     if (s == null) return d;
@@ -39,7 +51,11 @@ public class Parser {
     return d;
   }
 
-  // Conversion String en temps (exprimé en nombre de minutes depuis minuit)
+  /**
+   * Convertit une chaîne de caractère en nombre de minutes de jour
+   * @param s Chaîne à convertir
+   * @return -1 si la chaîne ne correspond pas à un format d'heure (hh:mm ou hhmm), nombre de minutes du jour sinon
+   */
   public static int toHeure(String s) {
     int h = Heure.HNULL;
     if (s == null) return h;
@@ -53,14 +69,25 @@ public class Parser {
     return h;
   }
 
-  // Crée un String contenant i espaces blancs
+  /**
+   * Crée une chaîne de caractères contenant un nombre défini d'espaces
+   * @param i Nombre d'espaces que doit contenir la chaîne
+   * @return Chaîne contenant le nombre d'espaces requis
+   */
   public static String blanks(int i) {
     StringBuilder b = new StringBuilder();
     while (i-- > 0) b.append(' ');
     return b.toString();
   }
 
-  // Aligne un String suivant le type t sur une ligne de length caractères
+  /**
+   * Aligne une chaîne de caractères sur une ligne d'une certaine longueur
+   * @param s Chaîne à aligner
+   * @param t Type de valeur qu'est supposé représenter la chaîne (permet de décider si l'alignement se fait à gauche, à droite ou centré)
+   * @param length Longueur de la ligne en nombre de caractères
+   * @return Chaîne alignée sur la ligne
+   * @see task.util.type.Type
+   */
   public static String align(String s, Type t, int length) {
     if (s == null) return blanks(length);
     int l = length - s.length();
@@ -72,7 +99,12 @@ public class Parser {
     return t.left() ? s + blanks(l) : blanks(l) + s;
   }
 
-  // Supprime la chaîne f du String s si s commence par f
+  /**
+   * Supprime un motif de début d'une chaîne de caractères
+   * @param s Chaîne de caractères à modifier
+   * @param f Motif de début
+   * @return La chaîne modifiée si la chaîne donnée contient bien le motif donné au début, la chaîne non modifiée sinon
+   */
   public static String removeFirst(String s, String f) {
     if (s == null)        return "";
     if (!s.startsWith(f)) return s;
@@ -80,7 +112,11 @@ public class Parser {
     return s.length() == l ? "" : s.substring(l);
   }
 
-  // Supprime les espaces blancs à gauche
+  /**
+   * Supprime les espaces à gauche
+   * @param s Chaîne à modifier
+   * @return Chaîne sans les espaces à gauche
+   */
   public static String lTrim(String s) {
     if (s == null || s.isEmpty()) return "";
     StringBuilder b = new StringBuilder(s);
@@ -88,7 +124,11 @@ public class Parser {
     return b.toString();
   }
 
-  // Supprime les espaces blancs à droite
+  /**
+   * Supprime les espaces à droite
+   * @param s Chaîne à modifier
+   * @return Chaîne sans les espaces à droite
+   */
   public static String rTrim(String s) {
     if (s == null || s.isEmpty()) return "";
     StringBuilder b = new StringBuilder(s);
@@ -96,12 +136,20 @@ public class Parser {
     return b.toString();
   }
 
-  // Supprime les espaces blancs à gauche et à droite
+  /**
+   * Supprime les espaces à gauche et à droite
+   * @param s Chaîne à modifier
+   * @return Chaîne sans les espaces inutiles
+   */
   public static String trim(String s) {
     return lTrim(rTrim(s));
   }
 
-  // Convertit une tableau en chaîne de caractères
+  /**
+   * Convertit un tableau en chaîne de caractères
+   * @param a Tableau de chaînes de caractères
+   * @return Chaîne contenant les entrées du tableau séparées par des espaces
+   */
   public static String toString(String[] a) {
     if (a == null || a.length == 0) return "";
     StringBuilder b = new StringBuilder();
