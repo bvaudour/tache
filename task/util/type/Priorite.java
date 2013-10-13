6a18bde7c4dd1e17187907923fcedcbbdead3167
bvaudour <bvaudour@myopera.com>
2013-10-13 15:44:12 +0200

    Définition priorité de tâches

--- task/util/type/Priorite.java
+++ task/util/type/Priorite.java
@@ -1,11 +1,15 @@
 package task.util.type;
 
 /**
- * Created with IntelliJ IDEA.
- * User: benjamin
- * Date: 06/10/13
- * Time: 15:34
- * To change this template use File | Settings | File Templates.
+ * L'enum <code>Priorite</code> définit la priorité d'une tâche :<br />
+ * <ul>
+ * <li><b>HIGH</b> : Priorité la plus importante</li>
+ * <li><b>MEDIUM</b> : Priorité moyenne</li>
+ * <li><b>LOW</b> : Priorité faible</li>
+ * <li><b>NONE</b> : Aucune priorité</li>
+ * </ul>
+ * @author Benjamin VAUDOUR
+ * @since 1.0
  */
 public enum Priorite {
 
@@ -14,6 +18,9 @@ public enum Priorite {
   LOW('L', 2),
   NONE('-', 3);
 
+  /**
+   * Expression régulière permettant d'identifier un format de priorité valide
+   */
   public static final String REGEX = "(H|h|M|m|L|l|-)";
 
   private static final Priorite[] _l = { HIGH, MEDIUM, LOW };
@@ -26,6 +33,11 @@ public enum Priorite {
     _i = i;
   }
 
+  /**
+   * Convertit un caractère en priorité
+   * @param c Caractère à convertir
+   * @return Priorité correspondante (Priorité nulle si aucune correspondance)
+   */
   public static Priorite getInstance(char c) {
     c = Character.toUpperCase(c);
     for (Priorite p : _l)
@@ -33,14 +45,29 @@ public enum Priorite {
     return NONE;
   }
 
+  /**
+   * Convertit une chaîne de caractères en priorité
+   * @param s Chaîne de caractères à convertir
+   * @return Priorité correspondante (Priorité nulle si aucune correspondance)
+   */
   public static Priorite getInstance(String s) {
     return (s != null && s.length() == 1) ? getInstance(s.charAt(0)) : NONE;
   }
 
+  /**
+   * Compare deux priorités
+   * @param p1 priorité 1
+   * @param p2 priorité 2
+   * @return 0 si les priorités sont identiques, -1 si p1 prioritaire sur p2, 1 sinon
+   */
   public static int compare(Priorite p1, Priorite p2) {
     return Integer.compare(p1._i, p2._i);
   }
 
+  /**
+   * Convertit la priorité en chaîne de caractères
+   * @return Chaîne correspondante
+   */
   @Override
   public String toString() {
     return String.valueOf(_c);
